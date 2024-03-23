f91108f392e7fb4c909f9ae7834932aa3382d1fb
kervines <kervinzero@gmail.com>
2024-03-23 13:51:30 -0300

    adicionado cdn font-awesome e estilos no css

--- js/appV1.js
+++ js/appV1.js
@@ -1,20 +1,39 @@
 const workspace = document.querySelector('.container-stickys');
 const btnAdd = document.querySelector('#add-sticky');
 
+function colorsRandom() {
+  const colors = ['#FF8080', '#FFCF96', '#F6FDC3', '#CDFAD5'];
+  const random = Math.floor(Math.random() * 4);
+  return colors[random];
+}
+
 getNotes().forEach((note) => {
-  const noteEl = createNote(note.id, note.content);
+  const noteEl = createNote(note.id, note.content, note.color);
   workspace.insertBefore(noteEl, btnAdd);
 });
 
-function createNote(id, content) {
+function createNote(id, content, color) {
+  const div = document.createElement('div');
+  div.classList.add('container-note');
+
   const element = document.createElement('textarea');
   element.classList.add('note');
-  element.value = content;
 
-  element.addEventListener('dblclick', () => {
+  element.value = content;
+  element.style.backgroundColor = color;
+  div.appendChild(element);
+
+  const btnClose = document.createElement('button');
+  btnClose.classList.add('close');
+  btnClose.innerHTML = '<i class="fa-solid fa-x"></i>';
+  div.appendChild(btnClose);
+  console.log(btnClose);
+
+  btnClose.addEventListener('click', () => {
     const warning = confirm('Do you want to delete this note?');
     if (warning) {
-      deleteNote(id, element);
+      deleteNote(id, div);
+      btnClose.remove();
     }
   });
 
@@ -22,7 +41,7 @@ function createNote(id, content) {
     updateNote(id, element.value);
   });
 
-  return element;
+  return div;
 }
 
 function deleteNote(id, element) {
@@ -40,8 +59,12 @@ function updateNote(id, content) {
 
 const addNote = () => {
   const notes = getNotes();
-  const noteObj = { id: Math.floor(Math.random() * 1000), content: '' };
-  const noteElement = createNote(noteObj.id, noteObj.content);
+  const noteObj = {
+    id: Math.floor(Math.random() * 1000),
+    content: '',
+    color: colorsRandom(),
+  };
+  const noteElement = createNote(noteObj.id, noteObj.content, noteObj.color);
   workspace.insertBefore(noteElement, btnAdd);
 
   notes.push(noteObj);
