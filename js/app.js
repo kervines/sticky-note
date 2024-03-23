e3b1f6b2ffc7a7fb93701ffb9a368b9d187e51de
kervines <kervinzero@gmail.com>
2024-03-22 23:20:05 -0300

    start functions js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,8 @@
 const container = document.querySelector('.container-stickys');
-const btnAddSticky = document.querySelector('#add-sticky');
+const btnAdd = document.querySelector('#add-sticky');
 
-const stickyLocalStorage = (id, text) => {
-  localStorage.setItem('sticky' + id, text);
+const addNote = () => {
+  const element = document.createElement('textarea');
+  container.appendChild(element);
 };
-
-let id = 0;
-const createStickyNote = () => {
-  const textArea = document.createElement('textarea');
-  textArea.setAttribute('cols', 30);
-  textArea.setAttribute('rows', 10);
-  textArea.style.resize = 'none';
-  textArea.classList.add('sticky');
-
-  textArea.addEventListener('change', () => {
-    stickyLocalStorage(id, textArea.value);
-  });
-  container.appendChild(textArea);
-  id++;
-  return textArea;
-};
-
-btnAddSticky.addEventListener('click', createStickyNote);
+btnAdd.addEventListener('click', addNote);
