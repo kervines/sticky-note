8c8f6e0719d593b2129c1695c7ceb96b4b8e904b
kervines <kervinzero@gmail.com>
2024-03-23 13:05:52 -0300

    adicionado script completo

--- js/app.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector('.container-stickys');
-const btnAdd = document.querySelector('#add-sticky');
-
-const addNote = () => {
-  const element = document.createElement('textarea');
-  container.appendChild(element);
-};
-btnAdd.addEventListener('click', addNote);
